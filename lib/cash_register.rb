3efa2f47f31037cb4ca5814ab0759de5254875b5
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-04 20:45:38 -0400

    changing @ to self.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,35 +7,24 @@ class CashRegister
         @items = []
         @last_transaction={}
     end
-    # def total
-    #     @total
-    # end
-    # def total= (newTotal)
-    #     @total = newTotal
-    # end
-    # def discount
-    #     @discount
-    # end
+    
     def add_item(title, price, quantity = 1)
         quantity.times do
-            @items.push(title)
+            self.items.push(title)
         end
         self.last_transaction[:title]=title
         self.last_transaction[:price]=price
         self.last_transaction[:quantity]=quantity
-        @total += price * quantity
+        self.total += price * quantity
     end
     def apply_discount
         if self.discount>0
-            @total -= @total * @discount/100
-            self.total = @total
-            "After the discount, the total comes to $#{@total}."
+            self.total -= self.total * @discount/100
+            # self.total = @total
+            "After the discount, the total comes to $#{self.total}."
         else
             "There is no discount to apply."
         end
-    end
-    def items
-        @items
     end
     def void_last_transaction
         self.last_transaction[:quantity].times do
